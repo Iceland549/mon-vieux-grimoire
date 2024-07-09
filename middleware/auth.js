5d6a45113a51ef0bfb159b24dffa3da57a1bac52
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-07-09 11:06:06 +0200

    Modify 'middleware/auth.js'

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,14 @@
 const jwt = require('jsonwebtoken');
  
 module.exports = (req, res, next) => {
+    console.log('Auth middleware called');
    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+        throw new Error('Authorization header is missing');
+        }
        const token = req.headers.authorization.split(' ')[1];
+       console.log('Token received:', token);
        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
        const userId = decodedToken.userId;
        req.auth = {
@@ -10,6 +16,7 @@ module.exports = (req, res, next) => {
        };
 	next();
    } catch(error) {
-       res.status(401).json({ error });
-   }
+        console.error('Auth middleware error:', error);
+        res.status(401).json({ error: error.message || 'Requête non authentifiée' });
+    }
 };
