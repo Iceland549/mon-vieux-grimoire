8cf472f931bd9c56583a31b5649d92365373693f
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-07-11 15:22:27 +0200

    Add bestrating and rating route to routes/book.js

--- routes/book.js
+++ routes/book.js
@@ -6,9 +6,9 @@ const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBooks);
 router.get('/:id', bookCtrl.getOneBook);
-//router.get('/bestrating', bookCtrl.getBestRatingBooks);
+router.get('/bestrating', bookCtrl.getBestRatingBooks);
 router.post('/', auth, multer, bookCtrl.createBook);
-//router.post('/:id/rating', auth, bookCtrl.rateBook);
+router.post('/:id/rating', auth, bookCtrl.rateBook);
 router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
