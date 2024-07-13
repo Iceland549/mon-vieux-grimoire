f3c8b89ac13d23eb7e1e3879f1a5f12116eedb79
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-07-13 10:34:20 +0200

    Modify app.js to include mongodb_uri

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const mongoose = require('mongoose');
 const path = require('path');
 const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/User');
 
 
-mongoose.connect('mongodb+srv://iceland549:jhVSuxlf0PCzyPxR@cluster0.rkc2po4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch((err) => console.log('Connexion à MongoDB échouée !', err));
 
 app.use(express.json());
 
 app.use((req, res, next) => {
+    console.log(`Received ${req.method} request for ${req.url}`);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
